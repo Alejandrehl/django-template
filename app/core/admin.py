5bfd167c7a588d0677761f56f875e175d44711cb
Alejandro Exequiel Hernandez Lara <alejandrehl@icloud.com>
2022-07-28 01:57:39 -0400

    save

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -13,6 +14,16 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        )
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
