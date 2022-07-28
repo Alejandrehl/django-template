6da3da37e1d0d2bae7f08d953e6fd34ba122a8ec
Alejandro Exequiel Hernandez Lara <alejandrehl@icloud.com>
2022-07-28 01:04:40 -0400

    passing all tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,6 +11,18 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+            )
+        })
+    )
 
 
 admin.site.register(models.User, UserAdmin)
